7330fab3ba87efe05ae736c7be577e90dcd967f4
tigerabrodi <tigerabrodi@gmail.com>
2021-06-05 12:54:05 +0200

    feat: setup function to declare theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { setup } from 'twind'
 import { App } from './App'
+
+setup({
+  theme: {
+    colors: {
+      white: 'F0F3F6',
+      darkBlue: '0A1625',
+      lightBlue: 'ACD5F2',
+    },
+    fontFamily: {
+      overpass: 'Overpass',
+    },
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
