9abb26b19ba90edf936afaa901e2f1ca644b4d60
tigerabrodi <tigerabrodi@gmail.com>
2021-06-06 16:39:49 +0200

    feat: extract footer into its own component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import { tw } from 'twind'
+
+export const Footer = () => (
+  <footer
+    className={tw`h-16 w-full border-t-2 border-white flex flex-row items-center justify-center`}
+  >
+    <p className={tw`text-blue.light`}>Built and designed by</p>
+    <a
+      href="https://github.com/tigerabrodi"
+      target="_blank"
+      rel="noreferrer"
+      className={tw`text-white underline pl-1`}
+    >
+      Tiger Abrodi
+    </a>
+  </footer>
+)
