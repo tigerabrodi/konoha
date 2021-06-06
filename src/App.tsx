2f572fa1408d11c1958b7e949c8827bdbb2d2f35
tigerabrodi <tigerabrodi@gmail.com>
2021-06-06 10:17:49 +0200

    feat: create home route with home component
    Types have been installed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
 import { Home } from './pages/Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-export const App = () => <Home />
+export const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Home} />
+    </Switch>
+  </Router>
+)
