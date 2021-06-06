31bb2b4bb7b753e1ea89f6b06d24016d9fade7d1
tigerabrodi <tigerabrodi@gmail.com>
2021-06-06 17:40:21 +0200

    feat(Footer): make border thicker

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { tw } from 'twind'
 
 export const Footer = () => (
   <footer
-    className={tw`h-16 w-full border-t-2 border-white flex flex-row items-center justify-center`}
+    className={tw`h-16 w-full border-t-4 border-white flex flex-row items-center justify-center`}
   >
     <p className={tw`text-blue.light`}>Built and designed by</p>
     <a
