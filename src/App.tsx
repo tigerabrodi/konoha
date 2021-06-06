8010a2dec31cae273c5639625b41bc740f6313d1
tigerabrodi <tigerabrodi@gmail.com>
2021-06-06 11:36:37 +0200

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -17,5 +17,18 @@ export const App = () => (
     <Switch>
       <Route path="/" exact component={Home} />
     </Switch>
+    <footer
+      className={tw`h-16 w-full border-t-2 border-white flex flex-row items-center justify-center`}
+    >
+      <p className={tw`text-blue.light`}>Built and designed by</p>
+      <a
+        href="https://github.com/tigerabrodi"
+        target="_blank"
+        rel="noreferrer"
+        className={tw`text-white underline pl-1`}
+      >
+        Tiger Abrodi
+      </a>
+    </footer>
   </Router>
 )
