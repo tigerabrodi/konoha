62875153939f5af11b7a4968bd83c72783245230
tigerabrodi <tigerabrodi@gmail.com>
2021-06-06 11:29:09 +0200

    feat: add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import { Home } from './pages/Home'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { ReactComponent as NinjaSVG } from 'assets/ninja.svg'
+import { tw } from 'twind'
 
 export const App = () => (
   <Router>
+    <nav className={tw`relative w-screen h-16`}>
+      <Link
+        to="/"
+        aria-label="To homepage"
+        className={tw`h-5/6 w-20 absolute top-0 left-0 mt-4 ml-2`}
+      >
+        <NinjaSVG aria-hidden="true" className={tw`w-full h-full`} />
+      </Link>
+    </nav>
     <Switch>
       <Route path="/" exact component={Home} />
     </Switch>
