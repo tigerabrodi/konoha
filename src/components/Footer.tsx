f91b8ed11382eb3dbd2d5038382c3e9cfa7da56e
tigerabrodi <tigerabrodi@gmail.com>
2021-06-09 14:16:40 +0200

    feat(Footer): add grouping styles and styles for tablet

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { tw } from 'twind'
 
 export const Footer = () => (
   <footer
-    className={tw`h-16 w-full border-t-4 border-white flex flex-row items-center justify-center`}
+    className={tw`h-16 w-full border(t-4 white) flex flex-row items-center justify-center tablet:(text-lg)`}
   >
     <p className={tw`text-blue.light`}>Built and designed by</p>
     <a
