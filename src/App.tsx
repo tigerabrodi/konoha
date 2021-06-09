75cf98f43c01a5f603e46dd03eb3b518c34af5ce
tigerabrodi <tigerabrodi@gmail.com>
2021-06-09 15:14:57 +0200

    feat: setup urql and client

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { Home } from 'pages/Home'
 import { CharacterDetail } from 'pages/CharacterDetail'
+import { createClient, Provider } from 'urql'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Footer } from 'components/Footer'
 import { Navigation } from 'components/Navigation'
 
+const client = createClient({
+  url: 'https://graphql.anilist.co/',
+  fetchOptions: {
+    method: 'POST',
+  },
+})
+
 export const App = () => (
-  <Router>
-    <Navigation />
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/character" exact component={CharacterDetail} />
-    </Switch>
-    <Footer />
-  </Router>
+  <Provider value={client}>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/character" exact component={CharacterDetail} />
+      </Switch>
+      <Footer />
+    </Router>
+  </Provider>
 )
